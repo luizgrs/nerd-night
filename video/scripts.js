bccda6a9ae3fbc5a877bc0e3ead19bd166680248
Luiz Soares <luizgrs@gmail.com>
2012-11-24 00:59:39 -0200

    initiating gamepad

--- video/scripts.js
+++ video/scripts.js
@@ -1,11 +1,14 @@
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+var gamepadSupportAvailable = !!navigator.webkitGetGamepads || !!navigator.webkitGamepads;
 window.URL = window.URL || window.webkitURL;
 window.addEventListener('load', load);
     
 function load()
 {   
     if(!navigator.getUserMedia)
-        alert('no web-rtc');    
+        alert('no web-rtc support');
+    else if(!gamepadSupportAvailable)
+        alert('no gamepad support');
     else
     {
         var stream = navigator.getUserMedia({video:true, audio: true}, cameraToVideo, error);
@@ -22,6 +25,6 @@ function cameraToVideo(localMediaStream) {
    var video = document.querySelector('video');
    video.src = window.URL.createObjectURL(localMediaStream);
    video.onloadedmetadata = function(e) {
-      video.play();
+      
    };
 }
