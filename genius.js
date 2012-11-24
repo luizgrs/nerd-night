46b3f64cfef8ef318fbc46f48483957f3ff93d92
eduardo <eduardo.mps@gmail.com>
2012-11-24 05:54:18 -0200

    genius + gamepad

--- genius.js
+++ genius.js
@@ -1,23 +1,59 @@
-function activate(idx){
+buttonPressed = function (idx){
 	var div = "";
-	switch(idx){
-		case 0: div="genius-blue"; break;
-		case 1: div="genius-red"; break;
-		case 2: div="genius-green"; break;
-		case 3: div="genius-yellow"; break;
+	if(idx&1){
+		div="genius-green";
+		document.getElementById(div).setAttribute("class", "active");
+		document.getElementById(div).className =  "active";
 	}
-	document.getElementByID(div).setAttribute("class", "active");
-	document.getElementByID(div).setAttribute("className", "active");
+	if(idx&2){
+		div="genius-red";
+		document.getElementById(div).setAttribute("class", "active");
+		document.getElementById(div).className =  "active";
+	}
+	if(idx&4){
+		div="genius-blue";
+		document.getElementById(div).setAttribute("class", "active");
+		document.getElementById(div).className =  "active";
+	}
+	
+	if(idx&8){
+		div="genius-yellow";
+		document.getElementById(div).setAttribute("class", "active");
+		document.getElementById(div).className =  "active";
+	}
 }
 
-function deactivate(idx){
+buttonUnpressed = function (idx){
 	var div = "";
-	switch(idx){
-		case 0: div="genius-blue"; break;
-		case 1: div="genius-red"; break;
-		case 2: div="genius-green"; break;
-		case 3: div="genius-yellow"; break;
+	if(idx&1){
+		div="genius-green";
+		document.getElementById(div).setAttribute("class", "");
+		document.getElementById(div).className =  "";
 	}
-	document.getElementByID(div).setAttribute("class", "");
-	document.getElementByID(div).setAttribute("className", "");
+	if(idx&2){
+		div="genius-red";
+		document.getElementById(div).setAttribute("class", "");
+		document.getElementById(div).className =  "";
+	}
+	if(idx&4){
+		div="genius-blue";
+		document.getElementById(div).setAttribute("class", "");
+		document.getElementById(div).className =  "";
+	}
+	
+	if(idx&8){
+		div="genius-yellow";
+		document.getElementById(div).setAttribute("class", "");
+		document.getElementById(div).className =  "";
+	}
+}
+
+var connection = function(){
+	var socket = new WebSocket('ws://LUIZGRS-PC:8181/websession');
+       socket.onopen = function() {
+           alert('handshake successfully established. May send data now...');
+       };
+       socket.onclose = function() {
+           alert('connection closed');
+       };
 }
