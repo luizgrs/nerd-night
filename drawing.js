e32fd17059b538e3002b3ea75596a44e6ca3126a
eduardo <eduardo.mps@gmail.com>
2012-11-24 00:58:50 -0200

    genius

--- drawing.js
+++ drawing.js
@@ -1,7 +1,14 @@
-var canvas = document.getElementById('cvs');
-// Check the element is in the DOM and the browser supports canvas
-if(canvas.getContext) {
-	// Initaliase a 2-dimensional drawing context
-	var context = canvas.getContext('2d');
-	//Canvas commands go here
+window.onload = function cvs(){
+	var canvas = document.getElementById('cvs');
+	// Check the element is in the DOM and the browser supports canvas
+	if(canvas.getContext) {
+		// Initaliase a 2-dimensional drawing context
+		var context = canvas.getContext('2d');
+		//Canvas commands go here
+		context.fillStyle = "rgb(0,0,0)";
+		//var h = context.height;
+		//var w = context.width;
+		context.arc(0,0,30,0,Math.PI*2,false);
+		context.fill();
+	}
 }
