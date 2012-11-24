1d4d53ec1423c03697a870655426350a72eda6c0
eduardo <eduardo.mps@gmail.com>
2012-11-24 00:34:04 -0200

    genius web

---
+++ drawing.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById('cvs');
+// Check the element is in the DOM and the browser supports canvas
+if(canvas.getContext) {
+	// Initaliase a 2-dimensional drawing context
+	var context = canvas.getContext('2d');
+	//Canvas commands go here
+}
